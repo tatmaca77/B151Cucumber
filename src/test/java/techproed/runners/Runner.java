7c04d76e5452a5f76a526595b9d250c8e82c9c92
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-08-01 11:38:36 +0200

    01.08.2023 Cucumber-FeatureFile

--- src/test/java/techproed/runners/Runner.java
+++ src/test/java/techproed/runners/Runner.java
@@ -1,13 +1,15 @@
 package techproed.runners;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 /**
   Runner class'i TestNG'deki .Xml File kullanimindaki gibi istedigimiz scenario'lari kullanacagimiz tag parametresi
   sayesinde calistirabiliriz. Ve plugin parametresi ile raporlar alabiliriz.
  */
-@RunWith(Cucumber.class)
+@RunWith(Cucumber.class) // --> Test calistirici Annotation
+@CucumberOptions()
 
 
 
@@ -19,3 +21,9 @@ import org.junit.runner.RunWith;
 
 public class Runner {
 }
+/**
+   @CucumberOptions() notasyonuna parametre olarak
+           features--> packega'in yolunu
+           glue--> stepdefinition package
+           tag--> calistirmak istedigimiz scenario'lari burda belirtiriz.
+ */
