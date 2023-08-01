7f02059695f03a1821c6f66c5bd4fd74231c92f9
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-08-01 11:35:27 +0200

    01.08.2023 Cucumber-FeatureFile

---
+++ src/test/java/techproed/runners/Runner.java
@@ -0,0 +1,21 @@
+package techproed.runners;
+
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+  Runner class'i TestNG'deki .Xml File kullanimindaki gibi istedigimiz scenario'lari kullanacagimiz tag parametresi
+  sayesinde calistirabiliriz. Ve plugin parametresi ile raporlar alabiliriz.
+ */
+@RunWith(Cucumber.class)
+
+
+
+
+
+
+
+
+
+public class Runner {
+}
