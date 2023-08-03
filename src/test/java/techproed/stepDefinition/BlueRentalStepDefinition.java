3c7dd0aa3da0422a0bb63335879103a9f1128ed8
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-08-03 12:41:45 +0200

    03.08.2023 Cucumber-DataTables- CucumberReports

--- src/test/java/techproed/stepDefinition/BlueRentalStepDefinition.java
+++ src/test/java/techproed/stepDefinition/BlueRentalStepDefinition.java
@@ -3,8 +3,11 @@ package techproed.stepDefinition;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.Keys;
 import techproed.pages.BlueRentalPage;
 import techproed.utilities.ConfigReader;
+import techproed.utilities.Driver;
+import techproed.utilities.ReusableMethods;
 
 public class BlueRentalStepDefinition {
     BlueRentalPage blueRentalPage = new BlueRentalPage();
@@ -17,7 +20,13 @@ public class BlueRentalStepDefinition {
 
     @And("kullanici verilen kullanici bilgileri ile login olur")
     public void kullaniciVerilenKullaniciBilgileriIleLoginOlur(DataTable data) {
-        blueRentalPage.emailBox.sendKeys(data.row(1).get(Integer.parseInt(ConfigReader.getProperty(String.valueOf(0)))));
+        blueRentalPage.emailBox.sendKeys(data.row(1).get(0), Keys.TAB,data.row(1).get(1),Keys.ENTER);
+        ReusableMethods.bekle(2);
+        Driver.getDriver().navigate().back();
+        ReusableMethods.bekle(2);
+        blueRentalPage.emailBox.sendKeys(data.row(2).get(0), Keys.TAB,data.row(2).get(1),Keys.ENTER);
+        ReusableMethods.bekle(2);
+
 
     }
 }
--- src/test/resources/features/day33_DataTables/US002_DataTableTest.feature
+++ src/test/resources/features/day33_DataTables/US002_DataTableTest.feature
@@ -5,7 +5,7 @@ Feature: US002 BlueRentalCar Login Islemi
     Then kullanici login butonuna tiklar
     But kullanici 2 saniye bekler
     And kullanici verilen kullanici bilgileri ile login olur
-      | email | password |
-      | mail1 | sifre1   |
-      | mail2 | sifre2   |
+      | email                         | password  |
+      | sam.walker@bluerentalcars.com | c!fas_art |
+      | kate.brown@bluerentalcars.com | tad1$Fas  |
     And sayfayi kapatir
